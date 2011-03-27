2c156c9f1866c094b6122505515124d27f955ac7
indisoluble <indisoluble@gmail.com>
2011-03-27 15:21:11 +0200

    Initial Commit

---
+++ costofliving/costoflivingAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  costoflivingAppDelegate.h
+//  costofliving
+//
+//  Created by Enrique de la Torre on 27/03/11.
+//  Copyright 2011 Enrique de la Torre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface costoflivingAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
